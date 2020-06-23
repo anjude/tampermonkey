ec313acad691cb134f62e588b251a983f1e1174c
anjude <aboy007262@163.com>
2020-06-23 12:24:58 +0800

    update

--- rain_tencent_class.js
+++ rain_tencent_class.js
@@ -11,7 +11,6 @@
 // @grant	       GM_registerMenuCommand
 // @grant	       GM_addStyle
 // @grant	       GM.setClipboard
-// @updateURL		 https://raw.githubusercontent.com/Anjude/tampermonkey/master/rain_tencent_class.js
 // ==/UserScript==
 (function () {
 	'use strict';
